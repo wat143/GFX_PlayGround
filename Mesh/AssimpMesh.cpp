2c7130cbb3a02b2169c5abbb71d220b8e7754e18
Wataru Mizuno <watarumizuno1128@gmail.com>
2023-05-26 16:25:36 +0900

    Mesh: Fix index data of AssimpMesh

--- Mesh/AssimpMesh.cpp
+++ Mesh/AssimpMesh.cpp
@@ -7,7 +7,7 @@ AssimpMesh::~AssimpMesh(){};
 // Assimp import mesh
 void AssimpMesh::processMesh(const aiMesh* mesh) {
   // Vertex Positions
-  for (int i = 0; i < mesh->mNumVertices; i++) {
+  for (unsigned int i = 0; i < mesh->mNumVertices; i++) {
     aiVector3D pos = mesh->mVertices[i];
     vertex_data.push_back(pos.x);
     vertex_data.push_back(pos.y);
@@ -16,31 +16,29 @@ void AssimpMesh::processMesh(const aiMesh* mesh) {
   // Normals
   if (!mesh->HasNormals())
     return;
-  for (int i = 0; i < mesh->mNumVertices; i++) {
+  for (unsigned int i = 0; i < mesh->mNumVertices; i++) {
     aiVector3D normal = mesh->mNormals[i];
     vertex_normal.push_back(normal.x);
     vertex_normal.push_back(normal.y);
     vertex_normal.push_back(normal.z);
   }
   // ToDo: Get Colors
-  unsigned int mColorChannel = mesh->GetNumColorChannels();
-  std::cout << mColorChannel << std::endl;
+
   // Index
   if (!mesh->HasFaces())
     return;
   int mFaceCount = mesh->mNumFaces;
   for (int i = 0; i < mFaceCount; i++) {
     aiFace& face = mesh->mFaces[i];
-    if (face.mNumIndices == 3) {
-      index_data.push_back(face.mIndices[0]);
-      index_data.push_back(face.mIndices[1]);
-      index_data.push_back(face.mIndices[2]);
+    for (unsigned int j = 0; j < face.mNumIndices; j++) {
+      index_num++;
+      index_data.push_back(face.mIndices[j]);
     }
   }
 }
 
 void AssimpMesh::processNode(const aiScene *scene) {
-  for (int i = 0; i < scene->mNumMeshes; i++)
+  for (unsigned int i = 0; i < scene->mNumMeshes; i++)
     processMesh(scene->mMeshes[i]);
 }
 
